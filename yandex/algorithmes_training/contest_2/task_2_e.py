8116aa464fb3bf23ceb1ca5d5a4c6546c4c4bbb1
Alexey <al913@mail.ru>
2024-03-12 23:12:52 +0300

    Stop at task 5

--- yandex/algorithmes_training/contest_2/task_2_e.py
+++ yandex/algorithmes_training/contest_2/task_2_e.py
@@ -1,11 +1,41 @@
+# n = int(input())
+# berries = [list(map(int, input().split())) + [i] for i in range(n)]
+# for i in range(n):
+#     for j in range(n):
+#         if berries[j][0] < berries[i][0]:
+#             berries[i], berries[j] = berries[j], berries[i]
+# print(berries)
+# for i in range(n):
+#     for j in range(n):
+#         if berries[j][1] > berries[i][1] and :
+#             berries[i], berries[j] = berries[j], berries[i]
+# print(berries)
+# sum_dist = 0
+# max_high = 0
+# for i in berries:
+#     sum_dist += i[0]
+#     if sum_dist > max_high:
+#         max_high = sum_dist
+#     sum_dist -= i[1]
+# print(max_high)
+# for i in berries:
+#     print(i[2] + 1, end=' ')
+
+
 n = int(input())
 berries = [list(map(int, input().split())) + [i] for i in range(n)]
-for i in range(n):
-    for j in range(n):
-        if berries[j][0] - berries[j][1] < berries[i][0] - berries[i][1]:
+for i in range(n-1):
+    for j in range(n-1):
+        if berries[j][0] - berries[j][1] + berries[j+1][1] < berries[i][0] - berries[i][1] + berries[i+1][0]:
             berries[i], berries[j] = berries[j], berries[i]
+            
 sum_dist = 0
-for i in berries[:-1]:
-    sum_dist += (i[0] - i[1])
-sum_dist += berries[-1][0]
-print(sum_dist)
+max_high = 0
+for i in berries:
+    sum_dist += i[0]
+    if sum_dist > max_high:
+        max_high = sum_dist
+    sum_dist -= i[1]
+print(max_high)
+for i in berries:
+    print(i[2] + 1, end=' ')
