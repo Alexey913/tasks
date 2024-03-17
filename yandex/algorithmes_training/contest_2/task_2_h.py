98928db0e1ca6619a094062d9a6e9ce142a28a4b
Alexey <al913@mail.ru>
2024-03-17 22:43:26 +0300

    contest 2

--- yandex/algorithmes_training/contest_2/task_2_h.py
+++ yandex/algorithmes_training/contest_2/task_2_h.py
@@ -8,44 +8,64 @@ def find_max(characters, exclude):
                         max_pow = [characters[i][j], [i, j]]
     return max_pow
 
-def find_max_row(characters, max_pow):
+def find_max_row(characters, max_pow, exclude):
     max_row = []
     for i in range(len(characters)):
-        if max_pow[0] in characters[i]:
-            new = sorted(characters[i], reverse=True)
-            if new > max_row:
-                max_row = new
-                max_el = [max_row[0], [i, characters[i].index(max_row[0])]]
+        if i != exclude[0]:
+            if max_pow[0] in characters[i]:
+                new = sorted(characters[i], reverse=True)
+                if new > max_row:
+                    max_row = new
+                    max_el = [max_row[0], [i, characters[i].index(max_row[0])]]
     return max_el
 
-def find_max_col(characters, max_pow):
+def find_max_col(characters, max_pow, exclude):
     max_col = []
     for i in range(len(characters[0])):
         temp = []
-        for j in range(len(characters)):
-            temp.append(characters[j][i])
-        if max_pow[0] in temp:
-            new = sorted(temp, reverse=True)
-            if new > max_col:
-                max_col = new
-                max_el =[max_col[0], [j, i]]
+        if i != exclude[1]:
+            for j in range(len(characters)):
+                if j != exclude[0]:
+                    temp.append(characters[j][i])
+            if max_pow[0] in temp:
+                new = sorted(temp, reverse=True)
+                if new > max_col:
+                    max_col = new
+                    max_el = [max_col[0], [j, i]]
     return max_el
 
 
 
 n, m = map(int, input().split())
 characters = [list(map(int, input().split())) for _ in range(n)]
-max_pow = find_max(characters, [-1, -1])
-max_without_row = find_max(characters, [max_pow[1][0], -1])
-max_without_col = find_max(characters, [-1, max_pow[1][1]])
-if max_without_col[0] >= max_without_row[0]:
-    max_pow = find_max_row(characters, max_pow)
-    row = max_pow[1][0]
-    max_pow = find_max(characters, [row, -1])
-    col = max_pow[1][1]
+# Максимальный элемент
+max_pow_common = find_max(characters, [-1, -1])
+
+# Максимальный элемент с учетом нескольких максимумов по строкам
+max_pow = find_max_row(characters, max_pow_common, [-1, -1])
+row_1 = max_pow[1][0]
+# Вычисление максимума без строки
+max_pow = find_max(characters, [row_1, -1])
+# Максимальный элемент с учетом нескольких максимумов по столбцам
+max_pow = find_max_col(characters, max_pow, [row_1, -1])
+col_1 = max_pow[1][1]
+# Оставшийся максимум
+max_pow = find_max(characters, [row_1, col_1])
+max_1 = max_pow[0]
+
+
+# Максимальный элемент с учетом нескольких максимумов по столбцам
+max_pow = find_max_col(characters, max_pow_common, [-1, -1])
+col_2 = max_pow[1][1]
+# Вычисление максимума без столбца
+max_pow = find_max(characters, [-1, col_2])
+# Максимальный элемент с учетом нескольких максимумов по столбцам
+max_pow = find_max_row(characters, max_pow, [-1, col_2])
+row_2 = max_pow[1][0]
+# Оставшийся максимум
+max_pow = find_max(characters, [row_2, col_2])
+max_2 = max_pow[0]
+if max_2 > max_1:
+    print(row_1 + 1, col_1 + 1)
 else:
-    max_pow = find_max_col(characters, max_pow)
-    col = max_pow[1][1]
-    max_pow = find_max(characters, [-1, col])
-    row = max_pow[1][0]
-print(row + 1, col + 1)
+    print(row_2 + 1, col_2 + 1)
---
+++ yandex/algorithmes_training/contest_2/task_2_i.py
@@ -0,0 +1,35 @@
+n = int(input())
+ships = [list(map(int, input().split())) for _ in range(n)]
+x = {i: 0 for i in range(1, n+1)}
+y = {i: 0 for i in range(1, n+1)}
+for ship in ships:
+    x[ship[1]] += 1
+    y[ship[0]] += 1
+common_steps = 0
+for k in y:
+    if y[k] == 0:
+        for i in range(1, len(y)-k+1):
+            if y[k+i] != 0:
+                y[k] += 1
+                y[k+i] -= 1
+                common_steps += i
+                break
+    else:
+        step = 1
+        while y[k] > 1:
+            if k - step > 0 and y[k-step] == 0:
+                y[k-step] = 1
+                y[k] -= 1
+                common_steps += 1
+            if k + step <= len(x) and y[k+step] == 0 and y[k] > 1:
+                y[k+step] = 1
+                y[k] -= 1
+                common_steps += 1
+            step += 1
+step_line = []
+for line in range(1, n+1):
+    temp_steps = 0
+    for k, v in x.items():
+        temp_steps += abs(line - k) * v
+    step_line.append(temp_steps)
+print(common_steps + min(step_line))
