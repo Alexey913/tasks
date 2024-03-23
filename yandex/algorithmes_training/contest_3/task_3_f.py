8d74cbf0d654e39997d267e86e1d07920206cfb1
Alexey Shevtsov <al913@mail.ru>
2024-03-23 16:06:21 +0300

    one more

--- yandex/algorithmes_training/contest_3/task_3_f.py
+++ yandex/algorithmes_training/contest_3/task_3_f.py
@@ -1,14 +1,61 @@
-from collections import defaultdict
+# dictionary = input().split()
+# words = input().split()
+# result = {}
+
+# for word in words:
+#     for d in dictionary:
+#         if word.startswith(d) and len(result.setdefault(word, d)) > len(d):
+#             result[word] = d
+#     print(result.setdefault(word, word), end =' ')
 
 
-dictionary = input().split()
-words = {i: '' for i in input().split()}
-result = defaultdict(str)
+# from collections import defaultdict
+
+
+# dictionary = input().split()
+# words = input().split()
+# result = defaultdict(list)
+
+# for i in dictionary:
+#     result[i[0]].append(i)
+
+# for word in words:
+#     if word[0] in result:
+#         for i in result[word[0]]:
+#             if word.startswith(i) and len(i) < len(word):
+#                 word = i
+#     print(word, end = ' ') 
+
+
+# from collections import defaultdict
+
+
+# dictionary = set(input().split())
+# words = input().split()
+# result = defaultdict(list)
+
+# for i in dictionary:
+#     result[i[0]].append(i)
+
+# for word in words:
+#     if word[0] in result:
+#         for i in range(1, min(101, len(word))):
+#             if word[:i] in result[word[0]]:
+#                 word = word[:i]
+#                 break
+#     print(word, end = ' ')
+
+
+dictionary = set(input().split())
+words = input().split()
+result = []
+
 
 for word in words:
-    for d in dictionary:
-        if word.startswith(d) and len(d) < len(result[word]):
-            result[word] = d
-        else:
-            result[word] = word
-print(result)
+    for i in range(1, min(101, len(word))):
+        if word[:i] in dictionary:
+            result.append(word[:i])
+            break
+    else:
+        result.append(word)
+print(*result)
---
+++ yandex/algorithmes_training/contest_3/task_3_g.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+
+n = int(input())
+coord = set()
+for _ in range(n):
+    coord.add(tuple(map(int, input().split())))
+result = defaultdict(list)
+for i in coord:
+    point_1 = i
+    if 
