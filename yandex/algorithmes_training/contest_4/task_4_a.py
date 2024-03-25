8677ada3708ee42d1dfe4f216329a65c34b79666
Alexey <al913@mail.ru>
2024-03-25 22:48:22 +0300

    contest 4 - 1, 2

--- yandex/algorithmes_training/contest_4/task_4_a.py
+++ yandex/algorithmes_training/contest_4/task_4_a.py
@@ -1,11 +1,9 @@
-from datetime import datetime
-
 def search_down(down: int, numbers: list[int]) -> int:
     l = 0
     r = len(numbers) - 1
     while r > l:
         mid = (l + r) // 2
-        if numbers[mid] <= down:
+        if numbers[mid] < down:
             l = mid + 1
         else:
             r = mid
@@ -17,7 +15,7 @@ def search_up(up: int, numbers: list[int]) -> int:
     r = len(numbers) - 1
     while r > l:
         mid = (l + r + 1) // 2
-        if numbers[mid] < up:
+        if numbers[mid] <= up:
             l = mid
         else:
             r = mid - 1
@@ -25,14 +23,10 @@ def search_up(up: int, numbers: list[int]) -> int:
 
 
 def find_len_between_limits(down: int, up: int, numbers: list[int]) -> int:
-    
-    # print(f'{down=}')
-    # print(f'{up=}')
-    # print(numbers)
-    if down == up:
-        return numbers.count(down)
     if down > up:
         down, up = up, down
+    if min_num > up or max_num < down:
+        return 0
     if min_num >= down:
         down_index = 0
     else:
@@ -41,40 +35,17 @@ def find_len_between_limits(down: int, up: int, numbers: list[int]) -> int:
         up_index = len(numbers) - 1
     else:
         up_index = search_up(up, numbers)
-    # print(f'{up_index=}')
-    # print(f'{down_index=}')
     return up_index - down_index + 1
 
 
-# n = int(input())
-# nums = sorted(list(map(int, input().split())))
-# k = int(input())
-# result = []
-# min_num = min(nums)
-# max_num = max(nums)
-# set_nums = set(nums)
-# for _ in range(k):
-#     result.append(find_len_between_limits(*map(int, input().split()), nums))
-# print(*result)
-
-
-start = datetime.now()
-res = []
 result = []
 with open('input.txt', 'r') as f:
     n = int(f.readline())
     nums = sorted(list(map(int, f.readline().split())))
-    set_nums = set(nums)
     min_num = min(nums)
     max_num = max(nums)
     k = int(f.readline())
     for _ in range(k):
         result.append(find_len_between_limits(*map(int, f.readline().split()), nums))
 f.close()
-print(result)
-# for i in res:
-#     print(*i)
-#     x = find_len_between_limits(*i, nums)
-#     print(f'{x=}')
-#     result.append(x)
-# print(*result)
+print(*result)
--- yandex/algorithmes_training/contest_4/task_4_b.py
+++ yandex/algorithmes_training/contest_4/task_4_b.py
@@ -1,18 +1,25 @@
+from fractions import Fraction
+
 def search(count_cell):
-    if count_cell == 0:
-        return 0
-    if count_cell < 6:
-        return 1
+    if count_cell == 0 or count_cell == 1:
+        return count_cell
     l = 0
     r = count_cell
     while r > l:
         mid = (l + r) // 2
-        
-        if check_ships(mid) > count_cell:
+        # формулы вывел, сложив все члены, вынес за скобки количество клеток большоего корабля,
+        # умножаем на формулу суммы и вычитаем сумму треугольных чисел + пробелы: формула суммы - 1
+        # nes_cells = (mid ** 2 * (mid + 1)) / 2 - (mid ** 3 - mid) / 3 + mid * (mid + 1) / 2 - 1
+        # упрощаем формулу
+        nes_cells_6 = mid * (mid ** 2 + 6 * mid + 5) - 6
+        if nes_cells_6 > count_cell * 6:
             r = mid
         else:
             l = mid + 1
     return l - 1
+
+n = int(input())
+print(search(n))
 
 
 # def check_ships(max_ship):
@@ -22,15 +29,15 @@ def search(count_cell):
 #     return count + max_ship - 1
 
 
-def check_ships(max_ship):
-    count = 0
-    for i in range(max_ship // 2):
-        count += 2 * (max_ship - i) * (i + 1) + max_ship
-    if max_ship % 2 != 0:
-        count += (max_ship - max_ship // 2) ** 2 + max_ship // 2 + 1
-    print(count)
-    return count + 1
+# def check_ships(max_ship):
+#     count = 0
+#     for i in range(max_ship // 2):
+#         count += 2 * (max_ship - i) * (i + 1) + max_ship
+#     if max_ship % 2 != 0:
+#         count += (max_ship - max_ship // 2) ** 2 + max_ship // 2 + 1
+#     print(count)
+#     return count + 1
 
-n = int(input())
-print(search(n))
+# n = int(input())
+# print(search(n))
 # print(check_ships(5))
